6d242b2684181d55446df5bcba6df7c0a80580b8
hasmishx <hasmishx@gmail.com>
2021-11-08 03:23:25 +0000

    first commit

--- coding.c
+++ coding.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(void) {
 /* This is my first program in C */
-printf("hello world!");
-printf("i love coding");
+printf("hello world!'\n");
+printf("i love coding\n");
 return (0);
 }
 
